578f850e272924fce0933cdf80b34f04372fb183
in1309 <miha.oblisar@informatika.si>
2021-10-13 13:29:32 +0200

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import Logo from "../components/Logo/Logo";
+import Navigation from "../components/Navigation/Navigation";
+import "tachyons";
+
+function App() {
+  return <Navigation />;
+}
+
+export default App;
