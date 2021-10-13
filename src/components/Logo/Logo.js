71a6d7911b0bac85cdff8a6efc59d1b9e9bd90e3
obleey <miha.oblishar@gmail.com>
2021-10-13 16:45:42 +0200

    Added Logo.png

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Tilt from "react-tilt";
-// import brain from "/.brain.png";
+import logo from "./Logo.png";
 import "./Logo.css";
 
 const Logo = () => {
@@ -12,7 +12,7 @@ const Logo = () => {
         style={{ height: 250, width: 250 }}
       >
         <div className="Tilt-inner pa3">
-          <img style={{ paddingTop: "5px" }} alt="logo" />
+          <img style={{ paddingTop: "5px" }} src={logo} alt="logo" />
         </div>
       </Tilt>
     </div>
