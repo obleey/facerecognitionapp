ba98a3db80dfa9b229b0ee9d2c3b60beb54d66ce
obleey <miha.oblishar@gmail.com>
2021-10-14 19:46:55 +0200

    Added ImageLinkForm.css, ImageLinkForm.js and Rank.js

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Rank = () => {
+  return <p>aaa</p>;
+};
+
+export default Rank;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Logo from "../components/Logo/Logo";
 import Navigation from "../components/Navigation/Navigation";
+import ImageLinkForm from "../components/ImageLinkForm/ImageLinkForm";
+import Rank from "../components/Rank/Rank";
 import "tachyons";
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
 }
