b2329b6152d7c8841f7e20d2c78b02c8a9013523
obleey <miha.oblishar@gmail.com>
2021-10-27 13:45:52 +0200

    validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,7 +30,7 @@ const Register = (props) => {
     })
       .then((response) => response.json())
       .then((user) => {
-        if (user) {
+        if (user.id) {
           loadUser(user);
           onRouteChange('home');
         }
